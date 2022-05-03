7edaa9d291cd75f993087430a83228768489a1ba
David McCreary <davidfmccreary@gmail.com>
2022-05-03 14:10:49 -0400

    add signup page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rx_vote/db_service.dart';
 import 'package:flutter_rx_vote/firebase_options.dart';
-import 'package:flutter_rx_vote/signup_bloc.dart';
+import 'package:flutter_rx_vote/person_data.dart';
+import 'package:flutter_rx_vote/signup_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,19 +21,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Rx Vote',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  final SignupBloc _signupBloc = SignupBloc();
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -40,77 +39,30 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: StreamBuilder<bool>(
-        stream: _signupBloc.isSubmitValid,
-        builder: (context, isValidSnap) {
-          return ElevatedButton(
-            onPressed: (isValidSnap.hasData && isValidSnap.data!) ? _signupBloc.submit : null,
-            child: const Text('Submit'),
-          );
-        }
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _nameField(context),
-            _emailField(context),
-            _passwordField(context),
+            StreamBuilder<List<Person>>(
+              stream: dbService.persons,
+              builder: (context, personsSnap) {
+                if (!personsSnap.hasData) {
+                  return const Text('No data');
+                }
+                return Text('${personsSnap.data!.length} persons');
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupPage()),
+                  );
+                },
+                child: const Text('Add Person'))
           ],
         ),
       ),
-    );
-  }
-
-    Widget _nameField(BuildContext context) {
-    return StreamBuilder(
-      stream: _signupBloc.name,
-      builder: (context, snapshot) {
-        return TextField(
-          onChanged: _signupBloc.changeName,
-          keyboardType: TextInputType.name,
-          decoration: InputDecoration(
-            hintText: 'Jim Jones',
-            labelText: 'Name',
-            errorText: snapshot.hasError ? snapshot.error.toString() : null,
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _emailField(BuildContext context) {
-    return StreamBuilder(
-      stream: _signupBloc.email,
-      builder: (context, snapshot) {
-        return TextField(
-          onChanged: _signupBloc.changeEmail,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: 'me@fun.com',
-            labelText: 'Email',
-            errorText: snapshot.hasError ? snapshot.error.toString() : null,
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _passwordField(BuildContext context) {
-    return StreamBuilder(
-      stream: _signupBloc.password,
-      builder: (context, snapshot) {
-        return TextField(
-          obscureText: true,
-          onChanged: _signupBloc.changePassword,
-          decoration: InputDecoration(
-            hintText: 'Password',
-            labelText: 'Password',
-            errorText: snapshot.hasError ? snapshot.error.toString() : null,
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/signup_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rx_vote/signup_bloc.dart';
+
+class SignupPage extends StatelessWidget {
+  SignupPage({Key? key}) : super(key: key);
+
+ final SignupBloc _signupBloc = SignupBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signup Page'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: StreamBuilder<bool>(
+        stream: _signupBloc.isSubmitValid,
+        builder: (context, isValidSnap) {
+          return ElevatedButton(
+            onPressed: (isValidSnap.hasData && isValidSnap.data!) ? _signupBloc.submit : null,
+            child: const Text('Submit'),
+          );
+        }
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _nameField(context),
+            _emailField(context),
+            _passwordField(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+    Widget _nameField(BuildContext context) {
+    return StreamBuilder(
+      stream: _signupBloc.name,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: _signupBloc.changeName,
+          keyboardType: TextInputType.name,
+          decoration: InputDecoration(
+            hintText: 'Jim Jones',
+            labelText: 'Name',
+            errorText: snapshot.hasError ? snapshot.error.toString() : null,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return StreamBuilder(
+      stream: _signupBloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: _signupBloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'me@fun.com',
+            labelText: 'Email',
+            errorText: snapshot.hasError ? snapshot.error.toString() : null,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return StreamBuilder(
+      stream: _signupBloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          obscureText: true,
+          onChanged: _signupBloc.changePassword,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.hasError ? snapshot.error.toString() : null,
+          ),
+        );
+      },
+    );
+  }
+}
