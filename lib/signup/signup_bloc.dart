995fa3c9b1ec9fc95242ee01ce42a71c27d1048c
David McCreary <davidfmccreary@gmail.com>
2022-05-05 16:51:18 -0400

    remove name and password transforms, live coding ready point #3

--- lib/signup/signup_bloc.dart
+++ lib/signup/signup_bloc.dart
@@ -3,7 +3,6 @@ import 'signup_validators.dart';
 import 'package:rxdart/rxdart.dart';
 
 class SignupBloc with SignupValidators {
-
   SignupBloc() {
     // _validateAllStreamsHaveDataAndNoErrors = ValidateAllStreamsHaveDataAndNoErrors()
     //                                           ..listen([
@@ -14,12 +13,11 @@ class SignupBloc with SignupValidators {
   }
 
   final _passwordSubject = BehaviorSubject<String>();
-  Stream<String> get password =>
-      _passwordSubject.stream.transform(validatePassword);
+  Stream<String> get password => _passwordSubject.stream;
   Function(String) get changeName => _nameSubject.sink.add;
 
   final _nameSubject = BehaviorSubject<String>();
-  Stream<String> get name => _nameSubject.stream.transform(validateName);
+  Stream<String> get name => _nameSubject.stream;
   Function(String) get changePassword => _passwordSubject.sink.add;
 
   // Stream<bool> get isSubmitValid => Rx.combineLatest3(name, email, password,
@@ -44,5 +42,4 @@ class SignupBloc with SignupValidators {
     _nameSubject.close();
     //_validateAllStreamsHaveDataAndNoErrors.dispose();
   }
-
 }
