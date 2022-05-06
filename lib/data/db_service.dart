1de06863fba70d1e3ad090bb6b743ba715004a69
David McCreary <davidfmccreary@gmail.com>
2022-05-06 12:51:59 -0400

    remove live coding

--- lib/data/db_service.dart
+++ lib/data/db_service.dart
@@ -26,15 +26,27 @@ class DatabaseService {
   final _personsSubject = BehaviorSubject<List<Person>>();
   Stream<List<Person>> get persons => _personsSubject.stream;
 
+<<<<<<< HEAD
 
   final _personsMapSubject = BehaviorSubject<Map<String, Person>>();
+=======
+  //TODO:  remove for shaft
+  final _personsMapSubject = BehaviorSubject<Map<String, Person>>();
+  //final _personsMapSubject = BehaviorSubject.seeded(<String, Person>{});
+>>>>>>> parent of 4f8c165 (safe)
   Stream<Map<String, Person>> get personsMap => _personsMapSubject.stream;
 
   final _pollsSubject = BehaviorSubject<List<Poll>>();
   Stream<List<Poll>> get polls => _pollsSubject.stream;
 
+<<<<<<< HEAD
 
   final _pollsMapSubject = BehaviorSubject<Map<String, Poll>>();
+=======
+  //TODO:  remove for shaft
+  final _pollsMapSubject = BehaviorSubject<Map<String, Poll>>();
+  //final _pollsMapSubject = BehaviorSubject.seeded(<String, Poll>{});
+>>>>>>> parent of 4f8c165 (safe)
   Stream<Map<String, Poll>> get pollsMap => _pollsMapSubject.stream;
 
   final _votesMapSubject = BehaviorSubject<Map<String, List<Vote>>>();
